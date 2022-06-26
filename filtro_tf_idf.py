255648667eaa27091fbcd8ec6b64fbaf0308dde6
Juan Antonio Ortiz Guerra <juanantonioortizguerra@gmail.com>
2022-06-26 20:13:40 +0200

    termino tf-idf

--- filtro_tf_idf.py
+++ filtro_tf_idf.py
@@ -13,14 +13,15 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.neighbors import KNeighborsClassifier
 
 vectorizer = TfidfVectorizer()
-
-def entrenar_tf_idf(clean_subjects):
+knc = KNeighborsClassifier(n_neighbors=1)
+def entrenar_tf_idf(clean_subjects,number_spams, number_ham):
     y = []
-    for i in range(11):  #9494 correos de entrenamiento que son spam
+    for i in range(number_spams):  #9494 correos de entrenamiento que son spam
         y.append(1)
-    for i in range(11): #10828 correos del conjunto de entrenamiento 
+    for i in range(number_ham): #10828 correos del conjunto de entrenamiento 
         y.append(0)
     y = np.array(y)
+    
     #A continuación, leemos los correos del conjunto de validación y los limpiamos para eliminar símbolos de puntuación y "stopwords".
     spam_subjects_val = readEmails("Enron-Spam-Splited/val/no_deseado/")
     ham_subjects_val = readEmails("Enron-Spam-Splited/val/legítimo/")
@@ -28,12 +29,13 @@ def entrenar_tf_idf(clean_subjects):
     clean_ham_subjects_val = [cleanText(s) for s in ham_subjects_val]
     clean_subjects_val = np.concatenate((clean_spam_subjects_val, clean_ham_subjects_val))
     print(clean_subjects_val)
+    vectors = vectorizer.fit_transform(clean_subjects)
+    knc.fit(vectors, y)
     #Tokenizamos las palabras encontradas en el conjunto de validación, y le pedimos al modelo que nos muestre la clasificación realizada de los 50 primeros correos (que deberían ser spam).
     x_val = vectorizer.transform(clean_subjects_val).toarray()
-    vectors = vectorizer.fit_transform(clean_subjects)
+    
     print(vectors.toarray())
-    knc = KNeighborsClassifier(n_neighbors=1)
-    knc.fit(vectors, y)
+   
     print(knc.predict(vectors.toarray()[0:50]))
     y_pred_nb = knc.predict(x_val)     # Vector con la clasificación del filtro
     y_true_nb = []                     # Vector con los resultados que debería dar el filtro
@@ -50,3 +52,13 @@ def entrenar_tf_idf(clean_subjects):
     plt.xlabel("y_pred_nb")
     plt.ylabel("y_true_nb")
     plt.show()
+def clasificador_tf_idf(url):
+    subject = read_email(url)
+    clean_subject = [cleanText(s) for s in subject]
+    print(subject)
+    x = vectorizer.transform(clean_subject).toarray()
+    res = knc.predict(x)
+    if res[0] == 0:
+        return False
+    else:
+        return True
--- spam.py
+++ spam.py
@@ -3,15 +3,17 @@ from nltk import download
 import itertools
 from lectura import readEmails, cleanText, read_email, readEmailsWithBody
 from filtro_naive_bayes import entrenar_modelo, clasificador_nb
+from filtro_tf_idf import entrenar_tf_idf, clasificador_tf_idf
 from filtro_tf_idf import entrenar_tf_idf
 import numpy as np
 import gc
 download('punkt', download_dir='.')
 download('stopwords')
 
-spam_subjects = readEmails("Enron-Spam-Splited/train/no_deseado/") #9494
-ham_subjects = readEmails("Enron-Spam-Splited/train/legítimo/")   #10828
-
+#spam_subjects = readEmails("Enron-Spam-Splited/train/no_deseado/") #9494
+#ham_subjects = readEmails("Enron-Spam-Splited/train/legítimo/")   #10828
+spam_subjects = readEmailsWithBody("Enron-Spam-Splited/train/no_deseado/") #9494
+ham_subjects = readEmailsWithBody("Enron-Spam-Splited/train/legítimo/")   #10828
 clean_spam_subjects = [cleanText(s) for s in spam_subjects]
 clean_ham_subjects = [cleanText(s) for s in ham_subjects]
 clean_subjects = np.concatenate((clean_spam_subjects, clean_ham_subjects), axis=0)
@@ -19,6 +21,7 @@ clean_subjects = np.concatenate((clean_spam_subjects, clean_ham_subjects), axis=
 entrenar_modelo(clean_subjects, len(clean_spam_subjects), len(clean_ham_subjects))
 result = clasificador_nb("Enron-Spam-Splited/train/no_deseado/0")
 print(result)
-#entrenar_tf_idf(clean_subjects)
+entrenar_tf_idf(clean_subjects, len(clean_spam_subjects), len(clean_ham_subjects))
 
-
+result = clasificador_tf_idf("Enron-Spam-Splited/train/no_deseado/0")
+print(result)
